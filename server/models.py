80a8f876c75d64c11ba6fadb372eb192c62aa413
Joeken10 <joekentafir@gmail.com>
2024-10-01 19:40:57 +0300

    Completed Flask-SQLAlchemy Lab 1

--- server/models.py
+++ server/models.py
@@ -1,15 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
 
+# Initialize SQLAlchemy
 db = SQLAlchemy()
 
 class Earthquake(db.Model, SerializerMixin):
     __tablename__ = "earthquakes"
     
+    # Define the columns for the Earthquake model
     id = db.Column(db.Integer, primary_key=True)
-    magnitude = db.Column(db.Float)
-    location = db.Column(db.String)
-    year = db.Column(db.Integer)
-    
+    magnitude = db.Column(db.Float, nullable=False)  # Magnitude of the earthquake
+    location = db.Column(db.String, nullable=False)   # Location of the earthquake
+    year = db.Column(db.Integer, nullable=False)      # Year of the earthquake
+
     def __repr__(self):
+        # Format the representation string
         return f"<Earthquake {self.id}, {self.magnitude}, {self.location}, {self.year}>"
